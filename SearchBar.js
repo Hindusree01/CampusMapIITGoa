be3fbced853d2eed816878d9a2bfdb6bb25c1465
Hindusree01 <105185628+Hindusree01@users.noreply.github.com>
2023-06-24 11:29:44 +0530

    Update SearchBar.js

--- SearchBar.js
+++ SearchBar.js
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import placesData from './iitgoaplaces.json';
+import './App.css';
 
 const SearchBar = (props) => {
   const [place, setPlace] = useState('');
   const [places, setPlaces] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
     setPlaces(placesData);
   }, []);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setPlace(value);
+
+    // Filter the places based on input value
+    const filteredPlaces = places.filter((place) =>
+      place.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setSuggestions(filteredPlaces);
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setPlace(suggestion.name);
+    setSuggestions([]);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,18 +35,34 @@ const SearchBar = (props) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <select
+      <input
+        className='text-input'
+        type="text"
         value={place}
-        onChange={(e) => setPlace(e.target.value)}
+        onChange={handleInputChange}
+        placeholder="Search"
+        style={{ fontSize: '16px', padding: '5px' }}
+      />
+      <button
+        className="custom-button"
+        type="submit"
+        style={{ fontSize: '16px', padding: '5px'}}
       >
-        <option value="">Select a place...</option>
-        {places.map((place) => (
-          <option key={place.name} value={place.name}>
-            {place.name}
-          </option>
-        ))}
-      </select>
-      <button type="submit">Search</button>
+        <i className="fa fa-search" aria-hidden="true"></i> {/* Replace with your desired icon */}
+      </button>
+
+      {suggestions.length > 0 && (
+        <ul className="suggestions-list">
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion.name}
+              onClick={() => handleSuggestionClick(suggestion)}
+            >
+              {suggestion.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </form>
   );
 };
