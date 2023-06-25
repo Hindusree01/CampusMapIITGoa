870a5746aee2f53e5301be3d86f5ee2368779244
Hindusree01 <105185628+Hindusree01@users.noreply.github.com>
2023-06-25 14:53:49 +0530

    Update SearchBar.js

--- SearchBar.js
+++ SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import placesData from './iitgoaplaces.json';
 import './App.css';
+import Direction from "./Directions";
 
 const SearchBar = (props) => {
   const [place, setPlace] = useState('');
@@ -32,37 +33,53 @@ const SearchBar = (props) => {
     props.onSearch(place);
     setPlace('');
   };
+  
+
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className='text-input'
-        type="text"
-        value={place}
-        onChange={handleInputChange}
-        placeholder="Search"
-        style={{ fontSize: '16px', padding: '5px' }}
-      />
-      <button
-        className="custom-button"
-        type="submit"
-      >
-        <i className="fa fa-search" aria-hidden="true"></i> {/* Replace with your desired icon */}
-      </button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className='text-input'
+          type="text"
+          value={place}
+          onChange={handleInputChange}
+          placeholder="Search"
+          style={{ fontSize: '16px', padding: '5px' }}
+        />
+        <button
+          className="custom-button"
+          type="submit"
+        >
+          <i className="fa fa-search" aria-hidden="true"></i> {/* Replace with your desired icon */}
+        </button>
+        <div className="direction-button-container">
+          <button className="direction-button" >
+            <i className="fas fa-directions"></i>
+          </button>
+        </div>
+      
+        {suggestions.length > 0 && (
+          <ul className="suggestions-list">
+            {suggestions.map((suggestion) => (
+              <li
+                key={suggestion.name}
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </form>
 
-      {suggestions.length > 0 && (
-        <ul className="suggestions-list">
-          {suggestions.map((suggestion) => (
-            <li
-              key={suggestion.name}
-              onClick={() => handleSuggestionClick(suggestion)}
-            >
-              {suggestion.name}
-            </li>
-          ))}
-        </ul>
-      )}
-    </form>
+
+      <div className='direction-bar'>
+        <div ref={props.mapContainerRef} id="map"></div>
+        <Direction mapContainer={props.mapContainerRef} />
+      </div>
+
+    </div>
   );
 };
 
