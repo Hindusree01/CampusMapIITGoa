6f4a6427864a4cca3882ef2f7b297db9fb3edba8
Hindusree01 <105185628+Hindusree01@users.noreply.github.com>
2023-09-06 12:24:27 +0530

    Update BuildingMarker.js

--- src/BuildingMarker.js
+++ src/BuildingMarker.js
@@ -6,55 +6,59 @@ import {Icon} from "leaflet";
 const icons = {
     Classrooms: new Icon({
         iconUrl: "https://cdn-icons-png.flaticon.com/512/185/185578.png",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
-    Offices: new Icon({
-        iconUrl: "https://www.freeiconspng.com/thumbs/office-icon/office-icon--insharepics-11.png",
-        iconSize: [25, 25]
+    Buildings: new Icon({
+        iconUrl: "https://static.vecteezy.com/system/resources/thumbnails/005/720/273/small/office-building-icon-two-point-perspective-buildings-illustration-isolated-on-white-background-free-vector.jpg",
+        iconSize: [17, 17]
+    }),
+    'Faculty Cabins': new Icon({
+        iconUrl: "https://thumbs.dreamstime.com/b/black-line-icon-faculty-bureau-division-black-line-icon-faculty-conference-academy-bureau-division-167849240.jpg",
+        iconSize: [17, 17]
     }),
     Labs: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbBUKCYyJXo4YGKtfQgpTfjmSM0fRDzp5olQ&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Recreation: new Icon({
-        iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbWfkn0p20ZOzJygbdMexz82T7D1yD1py7AQ&usqp=CAU",
-        iconSize: [25, 25]
+        iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbWfkn0p17ZOzJygbdMexz82T7D1yD1py7AQ&usqp=CAU",
+        iconSize: [17, 17]
     }),
     Hostel: new Icon({
-        iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL7DJ87FTOMYy5625HjLikY7UMNRksMwqNbQ&usqp=CAU",
-        iconSize: [25, 25]
+        iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL7DJ87FTOMYy5617HjLikY7UMNRksMwqNbQ&usqp=CAU",
+        iconSize: [17, 17]
     }),
     Wellness: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm2aOqhaEWXy9PPvUJsKncsphLDINn_DJDAg&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     'Security Office': new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpRcGwraKxLbXkn9xjb36e8jN5uXp1euU2cg&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Library: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxFmAZXUburEpbQ64t9Xc_ZqFwvAsNKkGpkA&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Workshop: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNda2zhD8AeoGbCSwtf-Gzy5lYAGs1itXb-A&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Dining: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS66g_41IcDqXFidRy5mmYGGR8xbv5t3E8cxA&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Temple: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhxZblpKwQGC-JWylxuapLegk0K_KvnQx8Q&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Canteen: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIUMEVLuUQMvb5eaBW3GzE_swwyoD6y7UbvQ&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     }),
     Parking: new Icon({
         iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw0BHGi6uM4MzVL4MxNSXbiMDw2iQTOmVxrw&usqp=CAU",
-        iconSize: [25, 25]
+        iconSize: [17, 17]
     })
     
 };
